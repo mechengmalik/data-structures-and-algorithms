87909eff30175d410b162bee1914b08c4afef436
MechEngMalik <mecheng.malik93@gmail.com>
2021-08-10 17:11:22 +0300

    d

--- javascript/code-challenges/challenges-02.test.js
+++ javascript/code-challenges/challenges-02.test.js
@@ -289,49 +289,49 @@ describe('Testing challenge 6', () => {
   });
 });
 
-// xdescribe('Testing challenge 7', () => {
-//   test('It should return an array containing the character code for each letter', () => {
-//     expect(charCode(['C', 'o', 'd', 'e', '3', '0', '1'])).toStrictEqual([ 67, 111, 100, 101, 51, 48, 49 ]);
-//     expect(charCode(['C', 'o', 'd', 'e', '3', '0', '1']).length).toStrictEqual(7);
-//   });
-// });
+xdescribe('Testing challenge 7', () => {
+  test('It should return an array containing the character code for each letter', () => {
+    expect(charCode(['C', 'o', 'd', 'e', '3', '0', '1'])).toStrictEqual([ 67, 111, 100, 101, 51, 48, 49 ]);
+    expect(charCode(['C', 'o', 'd', 'e', '3', '0', '1']).length).toStrictEqual(7);
+  });
+});
 
-// xdescribe('Testing challenge 8', () => {
-//   test('It should return an array containing the keys from an object', () => {
-//     expect(evenOdd([5, 8, 2, 6, 9, 13, 542, 541])).toStrictEqual([ 'odd', 'even', 'even', 'even', 'odd', 'odd', 'even', 'odd' ]);
-//     expect(evenOdd([5, 8, 2, 6, 9, 13, 542, 541]).length).toStrictEqual(8);
-//   });
+xdescribe('Testing challenge 8', () => {
+  test('It should return an array containing the keys from an object', () => {
+    expect(evenOdd([5, 8, 2, 6, 9, 13, 542, 541])).toStrictEqual([ 'odd', 'even', 'even', 'even', 'odd', 'odd', 'even', 'odd' ]);
+    expect(evenOdd([5, 8, 2, 6, 9, 13, 542, 541]).length).toStrictEqual(8);
+  });
 
-//   test('It should work with all odd numbers', () => {
-//     expect(evenOdd([1, 3, 5, 7, 9])).toStrictEqual([ 'odd', 'odd', 'odd', 'odd', 'odd' ]);
-//     expect(evenOdd([1, 3, 5, 7, 9]).length).toStrictEqual(5);
-//   });
+  test('It should work with all odd numbers', () => {
+    expect(evenOdd([1, 3, 5, 7, 9])).toStrictEqual([ 'odd', 'odd', 'odd', 'odd', 'odd' ]);
+    expect(evenOdd([1, 3, 5, 7, 9]).length).toStrictEqual(5);
+  });
 
-//   test('It should work with all even numbers', () => {
-//     expect(evenOdd([2, 4, 6, 8, 10])).toStrictEqual([ 'even', 'even', 'even', 'even', 'even' ]);
-//     expect(evenOdd([2, 4, 6, 8, 10]).length).toStrictEqual(5);
-//   });
+  test('It should work with all even numbers', () => {
+    expect(evenOdd([2, 4, 6, 8, 10])).toStrictEqual([ 'even', 'even', 'even', 'even', 'even' ]);
+    expect(evenOdd([2, 4, 6, 8, 10]).length).toStrictEqual(5);
+  });
 
-//   test('It should return the string "N/A" if a non-number is included in the array', () => {
-//     expect(evenOdd([5, 8, 2, 'hi'])).toStrictEqual([ 'odd', 'even', 'even', 'N/A' ]);
-//     expect(evenOdd([5, 8, 2, 'hi']).length).toStrictEqual(4);
-//   });
-// });
+  test('It should return the string "N/A" if a non-number is included in the array', () => {
+    expect(evenOdd([5, 8, 2, 'hi'])).toStrictEqual([ 'odd', 'even', 'even', 'N/A' ]);
+    expect(evenOdd([5, 8, 2, 'hi']).length).toStrictEqual(4);
+  });
+});
 
-// xdescribe('Testing challenge 9', () => {
-//   test('It should return an array containing only the ability names', () => {
-//     expect(extractAbilities(snorlaxAbilities.abilities)).toStrictEqual(['gluttony', 'cute charm', 'immunity']);
-//     expect(extractAbilities(snorlaxAbilities.abilities).length).toStrictEqual(3);
-//   });
-// });
+xdescribe('Testing challenge 9', () => {
+  test('It should return an array containing only the ability names', () => {
+    expect(extractAbilities(snorlaxAbilities.abilities)).toStrictEqual(['gluttony', 'cute charm', 'immunity']);
+    expect(extractAbilities(snorlaxAbilities.abilities).length).toStrictEqual(3);
+  });
+});
 
-// xdescribe('Testing challenge 10', () => {
-//   test('It should return an array containing objects with name and total values', () => {
-//     expect(extractStats(snorlaxStats.stats)).toStrictEqual([
-//       { name: 'speed', total: 35, },
-//       { name: 'special-defense', total: 112, },
-//       { name: 'special-attack', total: 74, },
-//     ]);
-//     expect(extractStats(snorlaxStats.stats).length).toStrictEqual(3);
-//   });
-// });
+xdescribe('Testing challenge 10', () => {
+  test('It should return an array containing objects with name and total values', () => {
+    expect(extractStats(snorlaxStats.stats)).toStrictEqual([
+      { name: 'speed', total: 35, },
+      { name: 'special-defense', total: 112, },
+      { name: 'special-attack', total: 74, },
+    ]);
+    expect(extractStats(snorlaxStats.stats).length).toStrictEqual(3);
+  });
+});
